1e7ac9a17f1b6480fb42cd5bc8a9136b7d82be01
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2021-08-09 17:21:23 +0100

    ongoing calibration

--- src/scripts/maternal_perinatal_analyses/calibration_run_all_modules.py
+++ src/scripts/maternal_perinatal_analyses/calibration_run_all_modules.py
@@ -26,7 +26,7 @@ from tlo.methods import (
 class MyTestScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 649
+        self.seed = 557
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2021, 1, 2)
         self.pop_size = 50000
