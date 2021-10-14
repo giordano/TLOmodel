283f8a9f3c596ddedd9f4d4972366abd9755661f
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-10-14 12:10:27 +0100

    set up new batch run

--- src/scripts/rti/rti_calibrate_kch_death_and_inc.py
+++ src/scripts/rti/rti_calibrate_kch_death_and_inc.py
@@ -21,10 +21,10 @@ class TestScenario(BaseScenario):
         super().__init__()
         self.seed = 12
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2020, 1, 1)
+        self.end_date = Date(2015, 1, 1)
         self.pop_size = 20000
         self.smaller_pop_size = 20000
-        self.number_of_samples_in_parameter_range = 6
+        self.number_of_samples_in_parameter_range = 4
         self.number_of_draws = self.number_of_samples_in_parameter_range ** 2
         self.runs_per_draw = 3
 
