e713440ab6c3e5b545df13897bef7d1eac99d9f6
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2021-11-09 15:47:43 +0000

    new scenario file

--- src/scripts/maternal_perinatal_analyses/preliminary_anc_4_test.py
+++ src/scripts/maternal_perinatal_analyses/preliminary_anc_4_test.py
@@ -3,11 +3,9 @@ from tlo import logging
 from tlo.scenario import BaseScenario
 from tlo.methods import (
     care_of_women_during_pregnancy,
-    contraception, dummy_contraception,
+    contraception,
     demography,
     depression,
-    dx_algorithm_adult,
-    dx_algorithm_child,
     enhanced_lifestyle,
     healthburden,
     healthseekingbehaviour,
@@ -26,11 +24,11 @@ from tlo.methods import (
 class MyTestScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 5567
+        self.seed = 16
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2021, 1, 2)
         self.pop_size = 30000
-        self.number_of_draws = 5
+        self.number_of_draws = 10
         self.runs_per_draw = 1
 
     def log_configuration(self):
@@ -53,19 +51,17 @@ class MyTestScenario(BaseScenario):
         return [
             demography.Demography(resourcefilepath=self.resources),
             contraception.Contraception(resourcefilepath=self.resources),
-            #dummy_contraception.DummyContraceptionModule(),
             enhanced_lifestyle.Lifestyle(resourcefilepath=self.resources),
             healthburden.HealthBurden(resourcefilepath=self.resources),
             healthsystem.HealthSystem(resourcefilepath=self.resources,
-                                      disable_and_reject_all=True),
+                                      service_availability=['*'],
+                                      ignore_cons_constraints=True),
             symptommanager.SymptomManager(resourcefilepath=self.resources),
             depression.Depression(resourcefilepath=self.resources),
             cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=self.resources),
             healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
             malaria.Malaria(resourcefilepath=self.resources),
             hiv.Hiv(resourcefilepath=self.resources),
-            dx_algorithm_adult.DxAlgorithmAdult(resourcefilepath=self.resources),
-            dx_algorithm_child.DxAlgorithmChild(resourcefilepath=self.resources),
             pregnancy_supervisor.PregnancySupervisor(resourcefilepath=self.resources),
             care_of_women_during_pregnancy.CareOfWomenDuringPregnancy(resourcefilepath=self.resources),
             labour.Labour(resourcefilepath=self.resources),
@@ -74,4 +70,6 @@ class MyTestScenario(BaseScenario):
         ]
 
     def draw_parameters(self, draw_number, rng):
-        return {}
+        return {
+            'PregnancySupervisor': {'anc_service_structure': [4, 4]}
+        }
