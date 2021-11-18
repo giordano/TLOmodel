57540b6c43f36aacc96d91939d5d01441e868a7e
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2021-11-18 09:56:31 +0000

    finalise scaling functions and scale linear models again in 2015 to ensure rates are correct going forward

--- src/scripts/maternal_perinatal_analyses/standard_mph_calibration.py
+++ src/scripts/maternal_perinatal_analyses/standard_mph_calibration.py
@@ -25,7 +25,7 @@ from tlo.scenario import BaseScenario
 class TestScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 777
+        self.seed = 888
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2021, 1, 1)
         self.pop_size = 30000
