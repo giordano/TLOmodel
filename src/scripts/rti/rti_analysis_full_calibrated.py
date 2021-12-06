e4a5c74f2080f64bd69b092e85e9803e14e52df6
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-12-06 12:12:32 +0000

    set up new batch run. Need to calibrate number of in-hospital mortality to different values of HSB

--- src/scripts/rti/rti_analysis_full_calibrated.py
+++ src/scripts/rti/rti_analysis_full_calibrated.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from tlo import Date, logging
 from tlo.methods import (
@@ -53,22 +54,30 @@ class TestScenario(BaseScenario):
         hsb_cutoff_max = self.upper_iss_value + 1
         hsb_cutoff_min = 1
         iss_cut_off_scores = range(hsb_cutoff_min, hsb_cutoff_max)
-        multiplication_factor_max = 1.325581395 + 0.25
-        multiplication_factor_min = 1.325581395 - 0.25
-        scale_factor = np.linspace(multiplication_factor_min, multiplication_factor_max,
-                                   num=self.number_of_samples_in_parameter_range)
-        grid = self.make_grid(
-            {'scale_factor': scale_factor,
-             'rt_emergency_care_ISS_score_cut_off': iss_cut_off_scores}
-        )
+        parameter_df = pd.DataFrame()
+        parameter_df['rt_emergency_care_ISS_score_cut_off'] = iss_cut_off_scores
+        ninj = [1, 2, 3, 4, 5, 6, 7, 8]
+        injury_distributions = \
+            [[ninj, [0.7093834795440467, 0.2061841889568684, 0.05992798112965075, 0.017418226588785873,
+                     0.00506265373502088, 0.0014714737295484586, 0.00042768774047753785, 0.00012430857560121887]],
+             [ninj, [0.7093834795440467, 0.2061841889568684, 0.05992798112965075, 0.017418226588785873,
+                     0.00506265373502088, 0.0014714737295484586, 0.00042768774047753785, 0.00012430857560121887]],
+             [ninj,  [0.7093834795440467, 0.2061841889568684, 0.05992798112965075, 0.017418226588785873,
+                      0.00506265373502088, 0.0014714737295484586, 0.00042768774047753785, 0.00012430857560121887]],
+             [ninj,  [0.7093834795440467, 0.2061841889568684, 0.05992798112965075, 0.017418226588785873,
+                      0.00506265373502088, 0.0014714737295484586, 0.00042768774047753785, 0.00012430857560121887]],
+             [ninj, [0.7612737346807732, 0.18174215061897694, 0.04338808474124165, 0.010358223951360018,
+                     0.002472863323338708, 0.000590357289495644, 0.0001409385330646141, 3.364686174939126e-05]]
+             ]
+        parameter_df['number_of_injured_body_regions_distribution'] = injury_distributions
+
         return {
             'RTI': {'prob_death_iss_less_than_9': grid['scale_factor'][draw_number] * (102 / 11650),
                     'prob_death_iss_10_15': grid['scale_factor'][draw_number] * (7 / 528),
                     'prob_death_iss_16_24': grid['scale_factor'][draw_number] * (37 / 988),
                     'prob_death_iss_25_35': grid['scale_factor'][draw_number] * (52 / 325),
                     'prob_death_iss_35_plus': grid['scale_factor'][draw_number] * (37 / 136),
-                    'rt_emergency_care_ISS_score_cut_off': int(grid['rt_emergency_care_ISS_score_cut_off'][draw_number])
-                    },
+                    'rt_emergency_care_ISS_score_cut_off': grid['rt_emergency_care_ISS_score_cut_off'][draw_number]},
             }
 
 
