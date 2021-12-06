cb8b6e71deb16cbfae147aba6e90ee407b45b4a7
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-12-06 10:38:36 +0000

    set up new batch run. Need to calibrate number of in-hospital mortality to different values of HSB

--- src/scripts/rti/rti_in_hospital_mortality_calibration.py
+++ src/scripts/rti/rti_in_hospital_mortality_calibration.py
@@ -22,8 +22,9 @@ class TestScenario(BaseScenario):
         self.end_date = Date(2020, 1, 1)
         self.pop_size = 20000
         self.smaller_pop_size = 20000
-        self.number_of_samples_in_parameter_range = 10
-        self.number_of_draws = self.number_of_samples_in_parameter_range
+        self.number_of_samples_in_parameter_range = 6
+        self.upper_iss_value = 6
+        self.number_of_draws = self.number_of_samples_in_parameter_range * self.upper_iss_value
         self.runs_per_draw = 3
 
     def log_configuration(self):
@@ -49,17 +50,24 @@ class TestScenario(BaseScenario):
 
 
     def draw_parameters(self, draw_number, rng):
-
+        hsb_cutoff_max = self.upper_iss_value + 1
+        hsb_cutoff_min = 1
+        iss_cut_off_scores = range(hsb_cutoff_min, hsb_cutoff_max)
         multiplication_factor_max = 1.325581395 + 0.25
         multiplication_factor_min = 1.325581395 - 0.25
         scale_factor = np.linspace(multiplication_factor_min, multiplication_factor_max,
                                    num=self.number_of_samples_in_parameter_range)
+        grid = self.make_grid(
+            {'scale_factor': scale_factor,
+             'rt_emergency_care_ISS_score_cut_off': iss_cut_off_scores}
+        )
         return {
-            'RTI': {'prob_death_iss_less_than_9': scale_factor[draw_number] * (102 / 11650),
-                    'prob_death_iss_10_15': scale_factor[draw_number] * (7 / 528),
-                    'prob_death_iss_16_24': scale_factor[draw_number] * (37 / 988),
-                    'prob_death_iss_25_35': scale_factor[draw_number] * (52 / 325),
-                    'prob_death_iss_35_plus': scale_factor[draw_number] * (37 / 136)},
+            'RTI': {'prob_death_iss_less_than_9': grid['scale_factor'][draw_number] * (102 / 11650),
+                    'prob_death_iss_10_15': grid['scale_factor'][draw_number] * (7 / 528),
+                    'prob_death_iss_16_24': grid['scale_factor'][draw_number] * (37 / 988),
+                    'prob_death_iss_25_35': grid['scale_factor'][draw_number] * (52 / 325),
+                    'prob_death_iss_35_plus': grid['scale_factor'][draw_number] * (37 / 136),
+                    'rt_emergency_care_ISS_score_cut_off': grid['rt_emergency_care_ISS_score_cut_off'][draw_number]},
             }
 
 
