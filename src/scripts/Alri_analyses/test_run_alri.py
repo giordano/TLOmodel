d4887a55c0c89e22bdd8c5634650895c02d69f04
Ines Lin <ines.lin@ucl.ac.uk>
2021-12-07 09:55:12 +0000

    commit test_run_alri

---
+++ src/scripts/Alri_analyses/test_run_alri.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+import datetime
+
+from tlo import Date, Simulation, logging
+from tlo.analysis.utils import parse_log_file
+from tlo.methods import (
+    chronicsyndrome,
+    contraception,
+    demography,
+    enhanced_lifestyle,
+    healthburden,
+    healthseekingbehaviour,
+    healthsystem,
+    symptommanager,
+    simplified_births, alri
+)
+
+
+# The Resource files [NB. Working directory must be set to the root of TLO: TLOmodel]
+outputpath = Path("./outputs")
+resourcefilepath = Path("./resources")
+
+# Create name for log-file
+datestamp = datetime.date.today().strftime("__%Y_%m_%d")
+
+# Create dict to capture the outputs
+output_files = dict()
+
+
+# Establish the simulation object
+start_date = Date(year=2010, month=1, day=1)
+end_date = Date(year=2010, month=12, day=31)
+popsize = 2000
+
+log_config = {
+    "filename": "Logfile",
+    "directory": "./outputs",
+    "custom_levels": {
+        "*": logging.WARNING,
+        "tlo.methods.alri": logging.INFO,
+        "tlo.methods.demography": logging.INFO,
+    }
+}
+
+sim = Simulation(start_date=start_date, seed=0, log_config=log_config, show_progress_bar=True)
+
+# Register the appropriate 'core' modules and do not let healthsystem constraints operate
+sim.register(
+    # Core Modules
+    demography.Demography(resourcefilepath=resourcefilepath),
+    simplified_births.SimplifiedBirths(resourcefilepath=resourcefilepath),
+    enhanced_lifestyle.Lifestyle(resourcefilepath=resourcefilepath),
+    symptommanager.SymptomManager(resourcefilepath=resourcefilepath),
+    healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=resourcefilepath),
+    healthburden.HealthBurden(resourcefilepath=resourcefilepath),
+
+    # The HealthSystem
+    healthsystem.HealthSystem(resourcefilepath=resourcefilepath, disable=True),
+
+    # Register disease modules of interest
+    alri.Alri(resourcefilepath=resourcefilepath)
+)
+
+# Run the simulation
+sim.make_initial_population(n=popsize)
+sim.simulate(end_date=end_date)
+
+# Read the results
+output = parse_log_file(sim.log_filepath)
