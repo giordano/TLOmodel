c9bc3eaf2daeb7600b751a691afe85a7758b939a
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2021-12-10 11:59:17 +0000

    fixed typo

---
+++ src/scripts/rti/full_calibrated_single_injury.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+
+from tlo import Date, logging
+from tlo.methods import (
+    demography,
+    enhanced_lifestyle,
+    healthburden,
+    healthseekingbehaviour,
+    healthsystem,
+    rti,
+    simplified_births,
+    symptommanager,
+)
+from tlo.scenario import BaseScenario
+
+
+class TestScenario(BaseScenario):
+    def __init__(self):
+        super().__init__()
+        self.seed = 12
+        self.start_date = Date(2010, 1, 1)
+        self.end_date = Date(2020, 1, 1)
+        self.pop_size = 20000
+        self.smaller_pop_size = 20000
+        self.number_of_draws = 3
+        self.runs_per_draw = 3
+
+    def log_configuration(self):
+        return {
+            'filename': 'rti_calibrated_single_injury_model_run.py',
+            'directory': './outputs',
+            'custom_levels': {
+                '*': logging.INFO,
+            }
+        }
+
+    def modules(self):
+        return [
+            demography.Demography(resourcefilepath=self.resources),
+            enhanced_lifestyle.Lifestyle(resourcefilepath=self.resources),
+            healthsystem.HealthSystem(resourcefilepath=self.resources, service_availability=['*']),
+            healthburden.HealthBurden(resourcefilepath=self.resources),
+            symptommanager.SymptomManager(resourcefilepath=self.resources),
+            healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
+            rti.RTI(resourcefilepath=self.resources),
+            simplified_births.SimplifiedBirths(resourcefilepath=self.resources)
+        ]
+
+
+    def draw_parameters(self, draw_number, rng):
+        hsb_cutoff_max = 4 + 1
+        hsb_cutoff_min = 2
+        iss_cut_off_scores = range(hsb_cutoff_min, hsb_cutoff_max)
+        parameter_df = pd.DataFrame()
+        parameter_df['rt_emergency_care_ISS_score_cut_off'] = iss_cut_off_scores
+        ninj = [1, 2, 3, 4, 5, 6, 7, 8]
+        injury_distributions = \
+            [[ninj, [1, 0, 0, 0, 0, 0, 0, 0]],
+             [ninj, [1, 0, 0, 0, 0, 0, 0, 0]],
+             [ninj, [1, 0, 0, 0, 0, 0, 0, 0]]
+             ]
+
+        parameter_df['number_of_injured_body_regions_distribution'] = injury_distributions
+        parameter_df['scale_factor'] = [1.875581395 * 1.07843, 1.875581395 * 1.1589978526983375,
+                                        1.775581395 * 1.1213015334257184]
+        scale_for_inc = [1.0373195691920685, 1.0215936563566232, 0.999956113910908]
+        current_inc = 0.00715091242587118
+        parameter_df['base_rate_injrti'] = np.multiply(current_inc, scale_for_inc)
+        return {
+            'RTI': {'prob_death_iss_less_than_9': parameter_df['scale_factor'][draw_number] * (102 / 11650),
+                    'prob_death_iss_10_15': parameter_df['scale_factor'][draw_number] * (7 / 528),
+                    'prob_death_iss_16_24': parameter_df['scale_factor'][draw_number] * (37 / 988),
+                    'prob_death_iss_25_35': parameter_df['scale_factor'][draw_number] * (52 / 325),
+                    'prob_death_iss_35_plus': parameter_df['scale_factor'][draw_number] * (37 / 136),
+                    'rt_emergency_care_ISS_score_cut_off': int(parameter_df['rt_emergency_care_ISS_score_cut_off'][
+                                                                   draw_number]),
+                    'number_of_injured_body_regions_distribution':
+                        parameter_df['number_of_injured_body_regions_distribution'][draw_number],
+                    'base_rate_injrti': parameter_df['base_rate_injrti'][draw_number]},
+            }
+
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+    scenario_run([__file__])
--- src/scripts/rti/rti_analysis_full_calibrated_plots.py
+++ src/scripts/rti/rti_analysis_full_calibrated_plots.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import json
+import squarify
 
 from tlo.analysis.utils import (
     extract_params,
@@ -163,30 +164,36 @@ expected_hsb_upper = 0.85
 expected_hsb_lower = 0.6533
 average_n_inj_in_kch = 7057 / 4776
 expected_in_hos_mort = (182 + 38) / (3840 + 1227 + 182 + 38)
+hsb_in_accepted_range = np.where((results_df['HSB'] > expected_hsb_lower) & (results_df['HSB'] < expected_hsb_upper))
+hsb_colors = ['lightsalmon' if i not in hsb_in_accepted_range[0] else 'darksalmon' for i in results_df['HSB'].index]
+inc_colors = ['lightsteelblue' if i not in hsb_in_accepted_range[0] else 'steelblue' for i in results_df['HSB'].index]
+in_hos_inj_colors = ['darkseagreen' if i not in hsb_in_accepted_range[0] else 'mediumseagreen' for i in
+                     results_df['HSB'].index]
+in_hos_inj_mort_colors = ['plum' if i not in hsb_in_accepted_range[0] else 'violet' for i in results_df['HSB'].index]
 fig = plt.figure(constrained_layout=True)
 # Use GridSpec for customising layout
 gs = fig.add_gridspec(nrows=2, ncols=2)
 ax1 = fig.add_subplot(gs[0, 0])
-ax1.bar(np.arange(len(results_df)), results_df['inc'], color='lightsteelblue', label='Model output')
-ax1.axhline(gbd_inc, color='b', label='GBD estimate', linestyle='dashed')
-ax1.set_ylabel('Incidence per 100,000 p.y.')
+ax1.bar(np.arange(len(results_df)), results_df['HSB'], color=hsb_colors, label='Model output')
+ax1.axhline(expected_hsb_upper, color='r', label='Upper HSB bound', linestyle='dashed')
+ax1.axhline(expected_hsb_lower, color='r', label='Upper HSB bound', linestyle='dashed')
 ax1.set_xticks(results_df.index)
 ax1.set_xticklabels(results_df['ISS cutoff score'])
-ax1.legend()
-ax1.set_title('Incidence of RTI')
+ax1.set_ylabel('Percent')
+ax1.set_title('Percent sought care')
+ax1.legend(loc='lower left')
 ax1.set_xlabel('ISS cut-off score')
 ax2 = fig.add_subplot(gs[0, 1])
-ax2.bar(np.arange(len(results_df)), results_df['HSB'], color='lightsalmon', label='Model output')
-ax2.axhline(expected_hsb_upper, color='r', label='Upper HSB bound', linestyle='dashed')
-ax2.axhline(expected_hsb_lower, color='r', label='Upper HSB bound', linestyle='dashed')
+ax2.bar(np.arange(len(results_df)), results_df['inc'], color=inc_colors, label='Model output')
+ax2.axhline(gbd_inc, color='b', label='GBD estimate', linestyle='dashed')
+ax2.set_ylabel('Incidence per 100,000 p.y.')
 ax2.set_xticks(results_df.index)
 ax2.set_xticklabels(results_df['ISS cutoff score'])
-ax2.set_ylabel('Percent')
-ax2.set_title('Percent sought care')
-ax2.legend(loc='lower left')
+ax2.legend()
+ax2.set_title('Incidence of RTI')
 ax2.set_xlabel('ISS cut-off score')
 ax3 = fig.add_subplot(gs[1, 0])
-ax3.bar(np.arange(len(results_df)), results_df['In_hos_inj'], color='darkseagreen', label='Model output')
+ax3.bar(np.arange(len(results_df)), results_df['In_hos_inj'], color=in_hos_inj_colors, label='Model output')
 ax3.axhline(average_n_inj_in_kch, color='g', label='Expected injuries p.p.', linestyle='dashed')
 ax3.set_xticks(results_df.index)
 ax3.set_xticklabels(results_df['ISS cutoff score'])
@@ -195,7 +202,7 @@ ax3.set_title('Average number of injuries per person')
 ax3.legend(loc='lower left')
 ax3.set_xlabel('ISS cut-off score')
 ax4 = fig.add_subplot(gs[1, 1])
-ax4.bar(np.arange(len(results_df)), results_df['In_hos_mort'], color='plum', label='Model output')
+ax4.bar(np.arange(len(results_df)), results_df['In_hos_mort'], color=in_hos_inj_mort_colors, label='Model output')
 ax4.axhline(expected_in_hos_mort, color='m', label='Expected in-hospital\nmortality', linestyle='dashed')
 ax4.set_xticks(results_df.index)
 ax4.set_xticklabels(results_df['ISS cutoff score'])
@@ -211,14 +218,15 @@ fig = plt.figure(constrained_layout=True)
 # Use GridSpec for customising layout
 gs = fig.add_gridspec(nrows=2, ncols=2)
 ax1 = fig.add_subplot(gs[0, 0])
-ax1.bar(np.arange(len(results_df)), results_df['inc'], color='lightsteelblue')
+ax1.bar(np.arange(len(results_df)), results_df['inc'], color=inc_colors)
+ax1.axhline(gbd_inc, color='b', linestyle='dashed')
 ax1.set_xticks(results_df.index)
 ax1.set_xticklabels(results_df['ISS cutoff score'])
 ax1.set_ylabel('Incidence per 100,000 p.y.')
 ax1.set_title('Incidence of RTI')
 ax1.set_xlabel('ISS cut-off score')
 ax2 = fig.add_subplot(gs[0, 1])
-ax2.bar(np.arange(len(results_df)), results_df['inc_death'], color='lightsalmon')
+ax2.bar(np.arange(len(results_df)), results_df['inc_death'], color=hsb_colors)
 ax2.set_xticks(results_df.index)
 ax2.set_xticklabels(results_df['ISS cutoff score'])
 ax2.set_ylabel('Incidence per 100,000 p.y.')
@@ -228,7 +236,7 @@ for idx, val in enumerate(results_df['inc_death']):
     ax2.text(idx, val + 3, f"{np.round(val, 2)}", rotation=90)
 ax2.set_ylim([0, max(results_df['inc_death']) + 20])
 ax3 = fig.add_subplot(gs[1, 0])
-ax3.bar(np.arange(len(results_df)), results_df['DALYs'], color='peachpuff')
+ax3.bar(np.arange(len(results_df)), results_df['DALYs'], color=in_hos_inj_colors)
 ax3.set_xticks(results_df.index)
 ax3.set_xticklabels(results_df['ISS cutoff score'])
 ax3.set_ylabel('DALYs')
@@ -239,14 +247,143 @@ for idx, val in enumerate(results_df['DALYs']):
 ax3.set_ylim([0, max(results_df['DALYs']) + 100000])
 ax4 = fig.add_subplot(gs[1, 1])
 ax4.bar(np.arange(len(results_df)), np.divide(results_df['inc_death'], results_df['inc']),
-        color='plum')
+        color=in_hos_inj_mort_colors)
 ax4.set_xticks(results_df.index)
 ax4.set_xticklabels(results_df['ISS cutoff score'])
 ax4.set_ylabel('CFR')
 ax4.set_title('Case fatality ratio')
 ax4.set_xlabel('ISS cut-off score')
-for idx, val in enumerate(np.divide(results_df['scaled_inc_death'], results_df['scaled_inc'])):
+for idx, val in enumerate(np.divide(results_df['inc_death'], results_df['inc'])):
     ax4.text(idx, val + 0.05, f"{np.round(val, 2)}", rotation=90)
 ax4.set_ylim([0, 0.4])
 plt.savefig("C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/FinalPaperOutput/results.png",
             bbox_inches='tight')
+plt.clf()
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('Model runs')
+ax1.set_ylabel('Incidence of death\nper 100,000 p.y.')
+ax1.bar(results_df.index, results_df['inc_death'], width=0.4, color='lightsalmon',
+        label='Incidence of death')
+ax1.set_xticks(np.add(results_df.index, 0.2))
+ax1.set_xticklabels(results_df['ISS cutoff score'])
+ax1.set_ylim([0, 80])
+# Adding Twin Axes
+
+ax2 = ax1.twinx()
+
+ax2.set_ylabel('Percent sought care')
+ax2.bar(np.add(results_df.index, 0.4), results_df['HSB'], width=0.4, color='lightsteelblue',
+        label='HSB')
+ax2.axhline(y=expected_hsb_lower, color='steelblue', linestyle='dashed', label='lower HSB\nboundary')
+ax2.axhline(y=expected_hsb_upper, color='lightskyblue', linestyle='dashed', label='upper HSB\nboundary')
+ax2.set_ylim([0, 1.1])
+ax1.set_title("The model's predicted incidence of death for \nvarying levels of health seeking behaviour")
+# Show plot
+ax1.legend(loc='upper left')
+ax2.legend()
+plt.savefig("C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/FinalPaperOutput/inc_death_vs_hsb.png",
+            bbox_inches='tight')
+plt.clf()
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('Model runs')
+ax1.set_ylabel('DALYs')
+ax1.bar(results_df.index, results_df['DALYs'], width=0.4, color='lightsalmon',
+        label='DALYs')
+ax1.set_xticks(np.add(results_df.index, 0.2))
+ax1.set_xticklabels(results_df['ISS cutoff score'])
+ax1.set_ylim([0, 4000000])
+# Adding Twin Axes
+
+ax2 = ax1.twinx()
+
+ax2.set_ylabel('Percent sought care')
+ax2.bar(np.add(results_df.index, 0.4), results_df['HSB'], width=0.4, color='lightsteelblue',
+        label='HSB')
+ax2.axhline(y=expected_hsb_lower, color='steelblue', linestyle='dashed', label='lower HSB\nboundary')
+ax2.axhline(y=expected_hsb_upper, color='lightskyblue', linestyle='dashed', label='upper HSB\nboundary')
+ax2.set_ylim([0, 1.1])
+ax1.set_title("The model's predicted incidence of death for \nvarying levels of health seeking behaviour")
+# Show plot
+ax1.legend(loc='upper left')
+ax2.legend()
+plt.savefig("C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/FinalPaperOutput/DALYs_vs_hsb.png",
+            bbox_inches='tight')
+plt.clf()
+gbd_data = pd.read_csv('resources/gbd/ResourceFile_Deaths_And_DALYS_GBD2019.csv')
+gbd_data = gbd_data.loc[gbd_data['measure_name'] == 'DALYs (Disability-Adjusted Life Years)']
+gbd_data = gbd_data.loc[gbd_data['Year'].isin(range(2010, 2020))]
+gbd_data = gbd_data.groupby('cause_name').sum()
+gbd_data = gbd_data.nlargest(11, 'GBD_Est')
+old_order_names = gbd_data.index
+old_order_values = gbd_data['GBD_Est'].values
+gbd_data = gbd_data.nlargest(10, 'GBD_Est')
+gbd_data.loc['Road injuries'] = [805800, 0, results_df.iloc[hsb_in_accepted_range[0]].DALYs.mean(), 0, 0]
+gbd_data = gbd_data.sort_values('GBD_Est', ascending=False)
+new_order_names = gbd_data.index
+new_order_values = gbd_data['GBD_Est'].values
+
+old_order_names = ['HIV/AIDS', 'Neonatal\ndisorders', 'Lower\nrespiratory\ninfections', 'Malaria',
+                   'Diarrheal\ndiseases', 'Tuberculosis', 'Congenital\nbirth\ndefects', 'Meningitis',
+                   'Malnutrition', 'Stroke', 'Road injuries']
+old_order_colors = ['linen', 'navajowhite', 'khaki', 'yellow', 'grey', 'rosybrown', 'red', 'lightsalmon', 'peachpuff',
+                    'lightsteelblue', 'seagreen']
+new_order_names = ['HIV/AIDS', 'Neonatal\ndisorders', 'Lower\nrespiratory\ninfections', 'Malaria',
+                   'Diarrheal\ndiseases', 'Tuberculosis', 'Congenital\nbirth defects', 'Road injuries', 'Meningitis',
+                   'Malnutrition', 'Stroke']
+new_order_colors = ['linen', 'navajowhite', 'khaki', 'yellow', 'grey', 'rosybrown', 'red', 'seagreen', 'lightsalmon',
+                    'peachpuff', 'lightsteelblue']
+fig = plt.figure(constrained_layout=True)
+# Use GridSpec for customising layout
+gs = fig.add_gridspec(nrows=2, ncols=1)
+ax1 = fig.add_subplot(gs[0, 0])
+squarify.plot(old_order_values, label=old_order_names, color=old_order_colors)
+ax1.axis('off')
+ax1.set_title('Total DALYs predicted from 2010-2019 by condition, GBD study')
+ax2 = fig.add_subplot(gs[1, 0])
+squarify.plot(new_order_values, label=new_order_names, color=new_order_colors)
+ax2.axis('off')
+ax2.set_title('Total DALYs predicted from 2010-2019 by condition with RTI model')
+plt.savefig("C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/FinalPaperOutput/DALY_rankings.png",
+            bbox_inches='tight')
+plt.clf()
+
+gbd_data = pd.read_csv('resources/gbd/ResourceFile_Deaths_And_DALYS_GBD2019.csv')
+gbd_data = gbd_data.loc[gbd_data['measure_name'] == 'DALYs (Disability-Adjusted Life Years)']
+gbd_data = gbd_data.loc[gbd_data['Year'].isin(range(2010, 2020))]
+gbd_data = gbd_data.groupby('cause_name').sum()
+gbd_data = gbd_data.nlargest(11, 'GBD_Est')
+old_order_names = gbd_data.index
+old_order_values = gbd_data['GBD_Est'].values
+gbd_data = gbd_data.nlargest(10, 'GBD_Est')
+gbd_data.loc['Road injuries'] = [805800, 0, results_df.iloc[hsb_in_accepted_range[0]].DALYs.mean(), 0, 0]
+gbd_data = gbd_data.sort_values('GBD_Est', ascending=False)
+new_order_names = gbd_data.index
+new_order_values = gbd_data['GBD_Est'].values
+new_order_colors = ['lightsalmon'] * len(old_order_names)
+new_rti_index = np.where((new_order_names == 'Road injuries'))
+new_order_colors[new_rti_index[0][0]] = 'gold'
+
+new_order_colors = ['lightsalmon'] * len(old_order_names)
+new_rti_index = np.where([val == 'Road injuries' for val in new_order_names])
+new_order_colors[new_rti_index[0][0]] = 'gold'
+
+old_rti_index = np.where([val == 'Road injuries' for val in old_order_names])
+old_order_colors = ['lightsteelblue'] * len(old_order_names)
+old_order_colors[old_rti_index[0][0]] = 'gold'
+fig = plt.figure(constrained_layout=True)
+# Use GridSpec for customising layout
+gs = fig.add_gridspec(nrows=2, ncols=1)
+ax1 = fig.add_subplot(gs[0, 0])
+ax1.barh(np.arange(len(old_order_names)), old_order_values, color=old_order_colors)
+ax1.set_yticks(np.arange(len(old_order_names)))
+ax1.set_yticklabels(old_order_names)
+ax1.set_title('GBD ranked total DALYs')
+ax2 = fig.add_subplot(gs[1, 0])
+ax2.barh(np.arange(len(new_order_names)), new_order_values, color=new_order_colors)
+ax2.set_xlabel('Total DALYs 2010-2019')
+ax2.set_yticks(np.arange(len(new_order_names)))
+ax2.set_yticklabels(new_order_names)
+ax2.set_title('New ranked total DALYs')
+plt.savefig(f"C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/FinalPaperOutput/"
+            f"New_DALY_rankings.png", bbox_inches='tight')
+
