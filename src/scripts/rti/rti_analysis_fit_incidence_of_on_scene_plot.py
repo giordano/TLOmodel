6ab1e0141c5e3c3479e73214f440b369f7316e6e
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2022-01-07 09:27:44 +0000

    Created files to analyse other data sources created new script to parameterise model to on scene mortality reported by police data

--- src/scripts/rti/rti_analysis_fit_incidence_of_on_scene_plot.py
+++ src/scripts/rti/rti_analysis_fit_incidence_of_on_scene_plot.py
@@ -19,7 +19,7 @@ from tlo.analysis.utils import (
 
 outputspath = Path('./outputs/rmjlra2@ucl.ac.uk/')
 # 0) Find results_folder associated with a given batch_file and get most recent
-results_folder = Path('outputs/rmjlra2@ucl.ac.uk/rti_analysis_fit_incidence_of_on_scene-2022-01-06T124740Z')
+results_folder = get_scenario_outputs('rti_analysis_fit_incidence_of_on_scene.py', outputspath)[-1]
 # look at one log (so can decide what to extract)
 log = load_pickled_dataframes(results_folder)
 
@@ -34,5 +34,18 @@ extracted_incidence_of_death_on_scene = extract_results(results_folder,
                                                         column="incidence of death on scene per 100,000",
                                                         index="date"
                                                         )
-on_scene_inc_death = summarize(extracted_incidence_of_death_on_scene, only_mean=True)
-on_scene_inc_death = on_scene_inc_death * 100000
+on_scene_inc_death = summarize(extracted_incidence_of_death_on_scene, only_mean=True).mean()
+target_on_scene_inc_death = 6
+closest_est_found = min(on_scene_inc_death, key=lambda x: abs(x - target_on_scene_inc_death))
+best_fit_idx = np.where(on_scene_inc_death == closest_est_found)[0][0]
+plt.bar(np.arange(len(on_scene_inc_death)), on_scene_inc_death, label='Model estimates', color='lightsalmon')
+plt.bar(best_fit_idx, on_scene_inc_death[best_fit_idx], color='peachpuff', label='best fit found')
+plt.axhline(target_on_scene_inc_death, color='gold', label='NRSC estimate')
+plt.xticks(np.arange(len(on_scene_inc_death)), params['value'].round(3))
+plt.ylabel('Incidence of on scene mortality per 100,000')
+plt.xlabel('% of crashes that result in on-scene mortality')
+plt.legend()
+plt.title("Calibration of the model's on scene mortality to\n"
+          "The National Road Safety Council's (NRSC) estimate")
+plt.savefig("C:/Users/Robbie Manning Smith/Pictures/TLO model outputs/FinalPaperOutput/DALYs_vs_hsb.png",
+            bbox_inches='tight')
