78c041b452995ccd5a44599c340d7a00b632091c
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2022-02-07 08:38:36 +0000

    update scenario files

--- src/scripts/maternal_perinatal_analyses/scenario_files/standard_mph_calibration.py
+++ src/scripts/maternal_perinatal_analyses/scenario_files/standard_mph_calibration.py
@@ -3,19 +3,16 @@ from tlo.methods import (
     care_of_women_during_pregnancy,
     contraception,
     demography,
-    depression,
     enhanced_lifestyle,
     healthburden,
     healthseekingbehaviour,
     healthsystem,
     hiv,
     labour,
-    malaria,
     newborn_outcomes,
     postnatal_supervisor,
     pregnancy_supervisor,
     symptommanager,
-    cardio_metabolic_disorders
 )
 from tlo.scenario import BaseScenario
 
@@ -23,16 +20,16 @@ from tlo.scenario import BaseScenario
 class TestScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 666
+        self.seed = 876
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2030, 1, 2)
-        self.pop_size = 30000
-        self.number_of_draws = 5
-        self.runs_per_draw = 1
+        self.end_date = Date(2021, 1, 2)
+        self.pop_size = 15000
+        self.number_of_draws = 1
+        self.runs_per_draw = 5
 
     def log_configuration(self):
         return {
-            'filename': 'normal_30k_pop',
+            'filename': '15k_pop_core_modules',
             "custom_levels": {  # Customise the output of specific loggers. They are applied in order:
                 "*": logging.WARNING,
                 "tlo.methods.demography": logging.INFO,
@@ -55,18 +52,17 @@ class TestScenario(BaseScenario):
             healthburden.HealthBurden(resourcefilepath=self.resources),
             healthsystem.HealthSystem(resourcefilepath=self.resources,
                                       service_availability=['*'],
-                                      ignore_cons_constraints=True),
+                                      cons_availability='all'),
             symptommanager.SymptomManager(resourcefilepath=self.resources),
-            depression.Depression(resourcefilepath=self.resources),
-            cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=self.resources),
             healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
-            malaria.Malaria(resourcefilepath=self.resources),
-            hiv.Hiv(resourcefilepath=self.resources),
             pregnancy_supervisor.PregnancySupervisor(resourcefilepath=self.resources),
             care_of_women_during_pregnancy.CareOfWomenDuringPregnancy(resourcefilepath=self.resources),
             labour.Labour(resourcefilepath=self.resources),
             postnatal_supervisor.PostnatalSupervisor(resourcefilepath=self.resources),
             newborn_outcomes.NewbornOutcomes(resourcefilepath=self.resources),
+
+            hiv.DummyHivModule(),
+
         ]
 
     def draw_parameters(self, draw_number, rng):
