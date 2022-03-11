884630d88218762e993f9c759c0f19578dd59833
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2022-03-11 11:32:57 +0000

    Updates to analysis scripts.

--- src/scripts/maternal_perinatal_analyses/calibration/run_calibration_scripts.py
+++ src/scripts/maternal_perinatal_analyses/calibration/run_calibration_scripts.py
@@ -5,7 +5,7 @@ from src.scripts.maternal_perinatal_analyses.calibration.output_all_key_outcomes
 
 for file in ['run_core_modules']:
     output_key_outcomes_from_scenario_file(scenario_filename=f'{file}.py',
-                                           pop_size='10k',
+                                           pop_size='30k',
                                            outputspath='./outputs/sejjj49@ucl.ac.uk/',
                                            sim_years=list(range(2010, 2021)),
                                            show_and_store_graphs=True)
