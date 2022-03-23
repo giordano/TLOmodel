88c3aac5d3649d719987335b92138f6cfbd049b7
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2022-03-23 09:15:59 +0000

    updating scenario file to run all modules together

--- src/scripts/maternal_perinatal_analyses/scenario_files/standard_mph_calibration.py
+++ src/scripts/maternal_perinatal_analyses/scenario_files/standard_mph_calibration.py
@@ -1,11 +1,14 @@
 from tlo import Date, logging
 from tlo.methods import (
+    alri,
     cardio_metabolic_disorders,
     care_of_women_during_pregnancy,
     contraception,
     demography,
     depression,
+    diarrhoea,
     enhanced_lifestyle,
+    epi,
     healthburden,
     healthseekingbehaviour,
     healthsystem,
@@ -15,7 +18,9 @@ from tlo.methods import (
     newborn_outcomes,
     postnatal_supervisor,
     pregnancy_supervisor,
+    stunting,
     symptommanager,
+    wasting
 )
 from tlo.scenario import BaseScenario
 
@@ -23,16 +28,16 @@ from tlo.scenario import BaseScenario
 class TestScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 666
+        self.seed = 111
         self.start_date = Date(2010, 1, 1)
-        self.end_date = Date(2030, 1, 2)
+        self.end_date = Date(2021, 1, 2)
         self.pop_size = 30000
         self.number_of_draws = 5
         self.runs_per_draw = 1
 
     def log_configuration(self):
         return {
-            'filename': 'normal_30k_pop',
+            'filename': 'all_modules_30k',
             "custom_levels": {  # Customise the output of specific loggers. They are applied in order:
                 "*": logging.WARNING,
                 "tlo.methods.demography": logging.INFO,
@@ -53,20 +58,28 @@ class TestScenario(BaseScenario):
             contraception.Contraception(resourcefilepath=self.resources),
             enhanced_lifestyle.Lifestyle(resourcefilepath=self.resources),
             healthburden.HealthBurden(resourcefilepath=self.resources),
-            healthsystem.HealthSystem(resourcefilepath=self.resources,
-                                      service_availability=['*'],
-                                      ignore_cons_constraints=True),
             symptommanager.SymptomManager(resourcefilepath=self.resources),
-            depression.Depression(resourcefilepath=self.resources),
-            cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=self.resources),
-            healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
-            malaria.Malaria(resourcefilepath=self.resources),
-            hiv.Hiv(resourcefilepath=self.resources),
+            healthsystem.HealthSystem(resourcefilepath=self.resources,
+                                      mode_appt_constraints=1,
+                                      cons_availability='default'),  # went set disable=true, cant check HSI queue
+            newborn_outcomes.NewbornOutcomes(resourcefilepath=self.resources),
             pregnancy_supervisor.PregnancySupervisor(resourcefilepath=self.resources),
             care_of_women_during_pregnancy.CareOfWomenDuringPregnancy(resourcefilepath=self.resources),
             labour.Labour(resourcefilepath=self.resources),
             postnatal_supervisor.PostnatalSupervisor(resourcefilepath=self.resources),
-            newborn_outcomes.NewbornOutcomes(resourcefilepath=self.resources),
+            healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
+
+            # Register all the modules that are reference in the maternal perinatal health suite (including their
+            # dependencies)
+            alri.Alri(resourcefilepath=self.resources),
+            hiv.Hiv(resourcefilepath=self.resources),
+            malaria.Malaria(resourcefilepath=self.resources),
+            cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=self.resources),
+            depression.Depression(resourcefilepath=self.resources),
+            stunting.Stunting(resourcefilepath=self.resources),
+            wasting.Wasting(resourcefilepath=self.resources),
+            diarrhoea.Diarrhoea(resourcefilepath=self.resources),
+            epi.Epi(resourcefilepath=self.resources)
         ]
 
     def draw_parameters(self, draw_number, rng):
