f9ba4c39ca65fdad02a4960bc674570966fda9e3
Robert Manning Smith <robbie.smith@ucl.ac.uk>
2022-05-13 16:07:23 +0100

    Set up for further rti calibration single injury

--- src/scripts/rti/full_calibrated_single_injury.py
+++ src/scripts/rti/full_calibrated_single_injury.py
@@ -23,9 +23,9 @@ class TestScenario(BaseScenario):
         self.end_date = Date(2020, 1, 1)
         self.pop_size = 20000
         self.smaller_pop_size = 20000
-        self.upper_iss_value = 6
-        self.number_of_draws = 6
-        self.runs_per_draw = 4
+        self.upper_iss_value = 10
+        self.number_of_draws = 10
+        self.runs_per_draw = 2
 
     def log_configuration(self):
         return {
@@ -62,8 +62,10 @@ class TestScenario(BaseScenario):
         ninj = [1, 2, 3, 4, 5, 6, 7, 8]
         injury_distributions = [[ninj, [1, 0, 0, 0, 0, 0, 0, 0]]] * len(parameter_df)
         parameter_df['number_of_injured_body_regions_distribution'] = injury_distributions
-        parameter_df['scale_factor'] = [1.27210261, 0.97760263, 0.91028567, 0.96303679, 0.82095107, 0.71205461]
-        scale_for_inc = [1.02456099, 1.02166648, 1.02467306, 1.00992407, 0.99241982, 0.99833061]
+        parameter_df['scale_factor'] = [1.27210261, 0.97760263, 0.91028567, 0.96303679, 0.82095107, 0.71205461,
+                                        0.71205461, 0.71205461, 0.71205461, 0.71205461]
+        scale_for_inc = [1.02456099, 1.02166648, 1.02467306, 1.00992407, 0.99241982, 0.99833061, 0.99833061, 0.99833061,
+                         0.99833061, 0.99833061]
         current_inc = 0.00715091242587118
         parameter_df['base_rate_injrti'] = np.multiply(current_inc, scale_for_inc)
         return {
