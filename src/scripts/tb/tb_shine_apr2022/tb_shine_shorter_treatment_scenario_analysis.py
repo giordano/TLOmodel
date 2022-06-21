db22401f224a6076746f4204a45f6e4cba59a8bd
Leila-Uwais <leila.uwais17@imperial.ac.uk>
2022-06-21 13:29:31 +0100

    updated analysis script

--- src/scripts/tb/tb_shine_apr2022/tb_shine_shorter_treatment_scenario_analysis.py
+++ src/scripts/tb/tb_shine_apr2022/tb_shine_shorter_treatment_scenario_analysis.py
@@ -26,37 +26,9 @@ info = get_scenario_info(results_folder)
 # extract the parameters that have varied over the set of simulations
 params = extract_params(results_folder)
 
-
-def make_plot(model=None, model_low=None, model_high=None):
-    assert model is not None
-
-    fic, ax = plt.subplots()
-    ax.plot(model.index, model.values, "-", color="r")
-
-    if (model_low is not None) and (model_high is not None):
-        ax.fill_between(model_low.index, model_low, model_high, color="r", alpha=0.2)
-
-
-# --------------------- AVERAGE WORKER TIME --------------------- #
-
-average_worker_time = summarize(
-    extract_results(
-        results_folder,
-        module="tlo.methods.healthsystem.summary",
-        key="Capacity",
-        column="average_Frac_Time_Used_Overall",
-        index="date",
-        do_scaling=False))
-
-make_plot(model=average_worker_time[0]["mean"],
-          model_low=average_worker_time[0]["lower"],
-          model_high=average_worker_time[0]["upper"])
-plt.plot(average_worker_time[1]["mean"])
-plt.title('Fraction of Health Worker Time Used')
-plt.xlabel('Year')
-plt.ylabel('Fraction of Health Worker Time Used (Averaged per year)')
-plt.legend(['Baseline Scenario', 'Shorter Treatment Scenario'])
-plt.show()
+data_who_tb_2020 = pd.read_excel(rfp / 'ResourceFile_TB.xlsx', sheet_name='WHO_activeTB2020')
+data_who_tb_2020.index = pd.to_datetime(data_who_tb_2020['year'], format='%Y')
+data_who_tb_2020 = data_who_tb_2020.drop(columns=['year'])
 
 
 # ------------------------------ EXTRACT HEALTHSYSTEM SUMMARY DATA  ------------------------------ #
@@ -85,15 +57,99 @@ def extract_healthsystem_summary(results_folder: Path, module: str, key: str, co
 
 
 # ---------------------------------------- ANALYSIS PLOTS ---------------------------------------- #
-# -----------------------------------------  CONSUMABLES ----------------------------------------- #
-cons = extract_healthsystem_summary(
+
+# -----------------------------------------  TB INCIDENCE ----------------------------------------- #
+
+new_active_tb = summarize(extract_results(
+    results_folder,
+    module="tlo.methods.tb",
+    key="tb_shine_data",
+    column="NewActiveTBCases",
+    index="date",
+    do_scaling=False))
+
+
+
+fig, ax = plt.subplots()
+plt.plot(data_who_tb_2020.index, data_who_tb_2020["estimated_inc_number_children"], label="WHO Estimates", color="b")
+plt.fill_between(data_who_tb_2020["estimated_inc_number_children_low"].index,
+                 data_who_tb_2020["estimated_inc_number_children_low"], data_who_tb_2020["estimated_inc_number_children_high"],
+                 color="b", alpha=0.2)
+plt.plot(new_active_tb.index, new_active_tb[0]["mean"], label="Scenario 0", color="r")
+plt.fill_between(new_active_tb[0]["lower"].index, new_active_tb[0]["lower"], new_active_tb[0]["upper"], color="r", alpha=0.2)
+plt.plot(new_active_tb.index, new_active_tb[1]["mean"], label="Scenario 4", color="g")
+plt.fill_between(new_active_tb[1]["lower"].index, new_active_tb[1]["lower"], new_active_tb[1]["upper"], color="g", alpha=0.2)
+plt.xlabel('Year')
+plt.ylabel('Number of New Active TB Cases')
+plt.title('New Active TB Cases (0 - 16 Years)')
+plt.legend()
+plt.show()
+
+# -----------------------------------------  TB DIAGNOSIS ----------------------------------------- #
+
+new_diagnosed_tb = summarize(extract_results(
+    results_folder,
+    module="tlo.methods.tb",
+    key="tb_shine_data",
+    column="NewDiagnosedTBCases",
+    index="date",
+    do_scaling=False))
+
+
+
+
+fig, ax = plt.subplots()
+plt.plot(data_who_tb_2020.index, data_who_tb_2020["new_cases_014"], label="WHO Estimates", color="b")
+plt.plot(new_diagnosed_tb.index, new_diagnosed_tb[0]["mean"], label="Scenario 0", color="r")
+plt.fill_between(new_diagnosed_tb[0]["lower"].index, new_diagnosed_tb[0]["lower"], new_diagnosed_tb[0]["upper"], color="r", alpha=0.2)
+plt.plot(new_diagnosed_tb.index, new_diagnosed_tb[1]["mean"], label="Scenario 4", color="g")
+plt.fill_between(new_diagnosed_tb[1]["lower"].index, new_diagnosed_tb[1]["lower"], new_diagnosed_tb[1]["upper"], color="g", alpha=0.2)
+plt.ylim(bottom=600)
+plt.xlabel('Year')
+plt.ylabel('Number of New Diagnosed TB Cases')
+plt.title('New Diagnosed TB Cases (0 - 16 Years)')
+plt.legend()
+plt.show()
+
+# -----------------------------------------  TB TREATMENT ----------------------------------------- #
+
+new_treated_tb = summarize(extract_results(
+    results_folder,
+    module="tlo.methods.tb",
+    key="tb_shine_data",
+    column="NewTreatedTBCases",
+    index="date",
+    do_scaling=False))
+
+
+
+fig, ax = plt.subplots()
+plt.plot(new_treated_tb.index, new_treated_tb[0]["mean"], label="Scenario 0", color="r")
+plt.fill_between(new_treated_tb[0]["lower"].index, new_treated_tb[0]["lower"], new_treated_tb[0]["upper"], color="r", alpha=0.2)
+plt.plot(new_treated_tb.index, new_treated_tb[1]["mean"], label="Scenario 4", color="g")
+plt.fill_between(new_treated_tb[1]["lower"].index, new_treated_tb[1]["lower"], new_treated_tb[1]["upper"], color="g", alpha=0.2)
+plt.ylim(bottom=300)
+plt.xlabel('Year')
+plt.ylabel('Number of New Treated TB Cases')
+plt.title('New Treated TB Cases (0 - 16 Years)')
+plt.legend()
+plt.show()
+
+# -----------------------------------------  CONSUMABLES - TREATMENT ----------------------------------------- #
+cons_treatment = extract_healthsystem_summary(
     results_folder,
     module="tlo.methods.healthsystem.summary",
     key="Consumables",
     column="Item_Available")
 
-plt.plot(cons)
-plt.title("Consumables Use Over Time")
+
+fig, ax = plt.subplots()
+plt.plot(cons_treatment.index, cons_treatment[0]["178"], label="Scenario 0: Child Tx", color="b")
+plt.plot(cons_treatment.index, cons_treatment[0]["179"], label="Scenario 0: Child Re-tx", color="r")
+plt.plot(cons_treatment.index, cons_treatment[1]["178"], label="Scenario 4: Child Tx", color="b", linestyle="dashed")
+plt.plot(cons_treatment.index, cons_treatment[1]["179"], label="Scenario 4: Child Re-tx", color="r", linestyle="dashed")
+plt.plot(cons_treatment.index, cons_treatment[1]["2675"], label="Scenario 4: Child Tx Shorter", color="g", linestyle="dashed")
+plt.title("Treatment Use Over Time")
 plt.xlabel("Year")
 plt.ylabel("Quantity")
 plt.show()
