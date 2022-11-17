4d8d16424af0abe76d032be6b2e830b8430157f2
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2022-11-17 16:15:02 +0000

    changes to progression matrix for htn disorders

--- src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
+++ src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
@@ -4,21 +4,21 @@ import met_need
 
 # create dict of some scenario 'title' and the filename of the associated title
 scenario_dict = {'Status Quo': 'baseline_scenario',
-                 'Intervention 1': 'increased_anc_scenario',
-                 'Intervention 2': 'anc_scenario_plus_cons_and_qual',
-                 'Sensitivity (min)': 'min_anc_sensitivity_scenario',
-                 'Sensitivity (max)': 'max_anc_sensitivity_scenario',
+                 'Intervention 1': 'bemonc',
+                 'Intervention 2': 'cemonc',
+                 'Sensitivity (min)': 'min_sba_sensitivity_analysis',
+                 'Sensitivity (max)': 'max_sba_sensitivity_analysis',
                  }
 
 # define key variables used within the analysis scripts
 intervention_years = list(range(2022, 2031))
 sim_years = list(range(2010, 2031))
 output_path = './outputs/sejjj49@ucl.ac.uk/'
-service_of_interest = 'anc'
+service_of_interest = 'sba'
 scen_colours = ['rosybrown', 'lightcoral', 'pink', 'sandybrown', 'maroon']
 
-met_need.met_need_and_contributing_factors_for_deaths(scenario_dict, output_path, intervention_years,
-                                                      service_of_interest)
+# met_need.met_need_and_contributing_factors_for_deaths(scenario_dict, output_path, intervention_years,
+#                                                       service_of_interest)
 # maternal_newborn_health_analysis_v2.run_maternal_newborn_health_analysis(
 #      scenario_file_dict=scenario_dict,
 #      outputspath=output_path,
@@ -28,12 +28,12 @@ met_need.met_need_and_contributing_factors_for_deaths(scenario_dict, output_path
 #      show_all_results=True,
 #      scen_colours=scen_colours)
 
-# compare_incidence_rates_between_scenarios_v2.compare_key_rates_between_multiple_scenarios(
-#     scenario_file_dict=scenario_dict,
-#     outputspath=output_path,
-#     sim_years=sim_years,
-#     intervention_years=intervention_years,
-#     service_of_interest=service_of_interest,
-#     scen_colours=scen_colours)
-#
-#
+compare_incidence_rates_between_scenarios_v2.compare_key_rates_between_multiple_scenarios(
+    scenario_file_dict=scenario_dict,
+    outputspath=output_path,
+    sim_years=sim_years,
+    intervention_years=intervention_years,
+    service_of_interest=service_of_interest,
+    scen_colours=scen_colours)
+
+
