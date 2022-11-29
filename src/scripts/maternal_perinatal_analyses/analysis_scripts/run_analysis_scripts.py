c899451e6a528d8f9bf4b3b1f3db9cdb5a2f5191
joseph.h.collins@ucl.ac.uk <joseph.h.collins@ucl.ac.uk>
2022-11-29 12:55:20 +0000

    changes to progression matrix for htn disorders

--- src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
+++ src/scripts/maternal_perinatal_analyses/analysis_scripts/run_analysis_scripts.py
@@ -57,24 +57,24 @@ output_path = './outputs/sejjj49@ucl.ac.uk/'
 
 
 
-for scenario_dict, service, colours in zip([scenario_dict1, scenario_dict2, scenario_dict3],
-                                           ['anc', 'sba', 'pnc'],
-                                           [['lightcoral', 'firebrick', 'red', 'chocolate', 'darkorange'],
-                                            ['cadetblue', 'deepskyblue', 'midnightblue', 'green', 'palegreen'],
-                                            ['plum', 'purple', 'deeppink', 'crimson', 'maroon']]):
+for scenario_dict, service, colours in zip([scenario_dict2],
+                                           ['sba'],
+                                           [#['lightcoral', 'firebrick', 'red', 'chocolate', 'darkorange']]):
+                                            ['cadetblue', 'deepskyblue', 'midnightblue', 'green', 'palegreen']]):
+                                            #['plum', 'purple', 'deeppink', 'crimson', 'maroon']]):
 
     service_of_interest = service
     scen_colours = colours
 
-    maternal_newborn_health_analysis_v2.run_maternal_newborn_health_analysis(
-         scenario_file_dict=scenario_dict,
-         outputspath=output_path,
-         sim_years=sim_years,
-         intervention_years=intervention_years,
-         service_of_interest=service_of_interest,
-         show_all_results=True,
-         scen_colours=scen_colours)
-
+    # maternal_newborn_health_analysis_v2.run_maternal_newborn_health_analysis(
+    #      scenario_file_dict=scenario_dict,
+    #      outputspath=output_path,
+    #      sim_years=sim_years,
+    #      intervention_years=intervention_years,
+    #      service_of_interest=service_of_interest,
+    #      show_all_results=True,
+    #      scen_colours=scen_colours)
+    #
     compare_incidence_rates_between_scenarios_v2.compare_key_rates_between_multiple_scenarios(
         scenario_file_dict=scenario_dict,
         outputspath=output_path,
@@ -83,5 +83,5 @@ for scenario_dict, service, colours in zip([scenario_dict1, scenario_dict2, scen
         service_of_interest=service_of_interest,
         scen_colours=scen_colours)
 
-    met_need.met_need_and_contributing_factors_for_deaths(scenario_dict, output_path, intervention_years,
-                                                          service_of_interest)
+    # met_need.met_need_and_contributing_factors_for_deaths(scenario_dict, output_path, intervention_years,
+    #                                                       service_of_interest)
