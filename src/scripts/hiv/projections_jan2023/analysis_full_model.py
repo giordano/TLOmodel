a263d692f9a59d9201331245ff94f2515b015853
tdm32 <t.mangal@imperial.ac.uk>
2023-02-14 16:44:38 +0000

    remove init_dependencies from deviance_measure.py

--- src/scripts/hiv/projections_jan2023/analysis_full_model.py
+++ src/scripts/hiv/projections_jan2023/analysis_full_model.py
@@ -23,8 +23,8 @@ resourcefilepath = Path("./resources")
 
 # %% Run the simulation
 start_date = Date(2010, 1, 1)
-end_date = Date(2012, 1, 1)
-popsize = 2000
+end_date = Date(2010, 2, 1)
+popsize = 20
 scenario = 0
 
 # set up the log config
