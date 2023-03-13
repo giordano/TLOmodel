c21bf625fc36a74cbb5604f14047b861d0718d34
mnjowe <emmanuelmnjowe@gmail.com>
2023-03-13 15:50:09 +0200

    added some tests to copd test file

--- tests/test_copd.py
+++ tests/test_copd.py
@@ -1,1 +1,16 @@
 """ a test file to ensure COPD module functions are producing the expected behaviors """
+
+
+def test_copd_initiation_and_progression():
+    """ A function to test copd initiation and progression. check that
+            1-  individuals less than 15 years have copd set to Nan
+            2-  individuals aged 15+ years have different levels of copd assigned """
+
+
+def test_copd_hsi_interactions():
+    """ A function to test if people are able to get care when presented with emergency symptoms of copd """
+
+
+def test_copd_deaths():
+    """ a function to test copd deaths. Here, we assign a copd death probability of 1 and want to see if all people
+    die from copd"""
