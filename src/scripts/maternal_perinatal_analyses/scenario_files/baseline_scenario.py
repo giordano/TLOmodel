d71178396fddc3166c4fcccaaff9a282e309de31
joehcollins <joseph.h.collins@ucl.ac.uk>
2023-04-06 08:49:23 +0100

    updated scenario files

--- src/scripts/maternal_perinatal_analyses/scenario_files/baseline_scenario.py
+++ src/scripts/maternal_perinatal_analyses/scenario_files/baseline_scenario.py
@@ -1,35 +1,12 @@
 from tlo import Date, logging
-from tlo.methods import (
-    alri,
-    cardio_metabolic_disorders,
-    care_of_women_during_pregnancy,
-    contraception,
-    demography,
-    depression,
-    diarrhoea,
-    enhanced_lifestyle,
-    epi,
-    healthburden,
-    healthseekingbehaviour,
-    healthsystem,
-    hiv,
-    labour,
-    malaria,
-    newborn_outcomes,
-    postnatal_supervisor,
-    pregnancy_supervisor,
-    stunting,
-    symptommanager,
-    tb,
-    wasting,
-)
 from tlo.scenario import BaseScenario
+from tlo.methods.fullmodel import fullmodel
 
 
 class BaselineScenario(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 661184
+        self.seed = 537184
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2031, 1, 1)
         self.pop_size = 250_000
@@ -60,33 +37,11 @@ class BaselineScenario(BaseScenario):
         }
 
     def modules(self):
-        return [demography.Demography(resourcefilepath=self.resources),
-                contraception.Contraception(resourcefilepath=self.resources),
-                enhanced_lifestyle.Lifestyle(resourcefilepath=self.resources),
-                healthburden.HealthBurden(resourcefilepath=self.resources),
-                symptommanager.SymptomManager(resourcefilepath=self.resources),
-                healthsystem.HealthSystem(resourcefilepath=self.resources,
-                                          mode_appt_constraints=1,
-                                          cons_availability='default'),
-                newborn_outcomes.NewbornOutcomes(resourcefilepath=self.resources),
-                pregnancy_supervisor.PregnancySupervisor(resourcefilepath=self.resources),
-                care_of_women_during_pregnancy.CareOfWomenDuringPregnancy(resourcefilepath=self.resources),
-                labour.Labour(resourcefilepath=self.resources),
-                postnatal_supervisor.PostnatalSupervisor(resourcefilepath=self.resources),
-                healthseekingbehaviour.HealthSeekingBehaviour(resourcefilepath=self.resources),
-
-                # Register all the modules that are reference in the maternal perinatal health suite (including their
-                # dependencies)
-                alri.Alri(resourcefilepath=self.resources),
-                hiv.Hiv(resourcefilepath=self.resources),
-                tb.Tb(resourcefilepath=self.resources),
-                malaria.Malaria(resourcefilepath=self.resources),
-                cardio_metabolic_disorders.CardioMetabolicDisorders(resourcefilepath=self.resources),
-                depression.Depression(resourcefilepath=self.resources),
-                stunting.Stunting(resourcefilepath=self.resources),
-                wasting.Wasting(resourcefilepath=self.resources),
-                diarrhoea.Diarrhoea(resourcefilepath=self.resources),
-                epi.Epi(resourcefilepath=self.resources)]
+        return fullmodel(
+            resourcefilepath=self.resources,
+            module_kwargs={"HealthSystem": {"mode_appt_constraints": 1,
+                                            'cons_availability': 'default'}}
+        )
 
     def draw_parameters(self, draw_number, rng):
         return {
