0569b28089d6f17d123e8156bd71ab2b4c983d37
nicx503 <nic503@york.ac.uk>
2023-05-08 19:10:03 +0100

    revised code to extract sample outputs-DALYs and mortality

--- src/scripts/hiv/projections_jan2023/output_BoD_0.py
+++ src/scripts/hiv/projections_jan2023/output_BoD_0.py
@@ -14,8 +14,8 @@ from tlo import Date
 
 
 resourcefilepath = Path("./resources")
-#outputpath = Path("./outputs")  # folder for convenience of storing outputs
-outputpath = Path("./outputs/nic503@york.ac.uk")
+outputpath = Path("./outputs")  # folder for convenience of storing outputs
+#outputpath = Path("./outputs/nic503@york.ac.uk")
 datestamp = datetime.date.today().strftime("__%Y_%m_%d")
 
 TARGET_PERIOD = (Date(2010, 1, 1), Date(2013, 12, 31))
@@ -95,52 +95,52 @@ sample_dalys.to_excel(outputpath / "sample_dalys10.xlsx")
  #ylds = output['tlo.methods.healthburden']['yld_by_causes_of_disability']
 print(f"expected ylls{output['tlo.methods.healthburden']['yll_by_causes_of_death_stacked']}")
 # yll_output = output['tlo.methods.healthburden']['yll_by_causes_of_death_stacked']
-
-# results_folder = Path("./outputs")
-# results_folder = Path("./outputs/nic503@york.ac.uk")
-
-num_deaths =extract_results(
-        outputpath,
-        module='tlo.methods.demography',
-        key='death',
-        custom_generate_series=get_num_deaths,
-        do_scaling=True
-    )
-num_dalys = extract_results(
-    outputpath,
-    module='tlo.methods.healthburden',
-    key='dalys_stacked',
-    custom_generate_series=get_num_dalys(),
-    do_scaling=True
-)
-
-num_deaths_summarized = summarize(num_deaths).loc[0].unstack()
-num_dalys_summarized = summarize(num_dalys).loc[0].unstack()
-
-#     # # TB deaths will exclude TB/HIV
-#     # # keep if cause = TB
-#     # keep = (deaths.cause == "TB")
-#     # deaths_TB = deaths.loc[keep].copy()
-#     # deaths_TB["year"] = deaths_TB.index.year  # count by year
-#     # tot_tb_non_hiv_deaths = deaths_TB.groupby(by=["year"]).size()
-#     # tot_tb_non_hiv_deaths.index = pd.to_datetime(tot_tb_non_hiv_deaths.index, format="%Y")
-
-# Plot for total number of DALYs from the scenario
-name_of_plot = f'Total DALYS, {target_period()}'
-fig, ax = make_plot(num_dalys_summarized / 1e6)
-ax.set_title(name_of_plot)
-ax.set_ylabel('DALYS (Millions)')
-fig.tight_layout()
-fig.savefig("DALY_graph.png")
-plt.show()
-
-# plot of total number of deaths from the scenario
-name_of_plot= f'Total Deaths, {target_period()}'
-fig, ax = make_plot(num_deaths_summarized / 1e6)
-ax.set_title(name_of_plot)
-ax.set_ylabel('Deaths (Millions)')
-fig.tight_layout()
-fig.savefig("Mortality_graph.png")
-plt.show()
-
-
+#
+# # results_folder = Path("./outputs")
+# # results_folder = Path("./outputs/nic503@york.ac.uk")
+#
+# num_deaths =extract_results(
+#         outputpath,
+#         module='tlo.methods.demography',
+#         key='death',
+#         custom_generate_series=get_num_deaths,
+#         do_scaling=True
+#     )
+# num_dalys = extract_results(
+#     outputpath,
+#     module='tlo.methods.healthburden',
+#     key='dalys_stacked',
+#     custom_generate_series=get_num_dalys(),
+#     do_scaling=True
+# )
+#
+# num_deaths_summarized = summarize(num_deaths).loc[0].unstack()
+# num_dalys_summarized = summarize(num_dalys).loc[0].unstack()
+#
+# #     # # TB deaths will exclude TB/HIV
+# #     # # keep if cause = TB
+# #     # keep = (deaths.cause == "TB")
+# #     # deaths_TB = deaths.loc[keep].copy()
+# #     # deaths_TB["year"] = deaths_TB.index.year  # count by year
+# #     # tot_tb_non_hiv_deaths = deaths_TB.groupby(by=["year"]).size()
+# #     # tot_tb_non_hiv_deaths.index = pd.to_datetime(tot_tb_non_hiv_deaths.index, format="%Y")
+#
+# # Plot for total number of DALYs from the scenario
+# name_of_plot = f'Total DALYS, {target_period()}'
+# fig, ax = make_plot(num_dalys_summarized / 1e6)
+# ax.set_title(name_of_plot)
+# ax.set_ylabel('DALYS (Millions)')
+# fig.tight_layout()
+# fig.savefig("DALY_graph.png")
+# plt.show()
+#
+# # plot of total number of deaths from the scenario
+# name_of_plot= f'Total Deaths, {target_period()}'
+# fig, ax = make_plot(num_deaths_summarized / 1e6)
+# ax.set_title(name_of_plot)
+# ax.set_ylabel('Deaths (Millions)')
+# fig.tight_layout()
+# fig.savefig("Mortality_graph.png")
+# plt.show()
+#
+#
