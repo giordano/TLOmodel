c6a58de07200e861443a374d10b1ba2d286e7405
sakshimohan <sakshi.mohan08@gmail.com>
2023-05-18 11:46:51 +0100

    create scenario for simulation with mode_appt_constraints = 2

---
+++ src/scripts/healthsystem/impact_of_cons_availability/impact_of_pharmacist_intervention_hard_hr_constraints.py
@@ -0,0 +1,58 @@
+"""
+This file defines a batch run to calculate the health effect of updated consumable availability estimates
+as a result of a supply chain intervention. The following scenarios are currently considered:
+1. Scenario 1: Assign pharmacists to manage drug stocks at all level 1a and 1b facilities.
+
+The batch runs are for a large population for a long time with all disease modules and full use of HSIs.
+
+Run on the batch system using:
+```tlo batch-submit src/scripts/healthsystem/impact_of_cons_availability/impact_of_pharmacist_intervention_hard_hr_constraints.py```
+
+or locally using:
+    ```tlo scenario-run src/scripts/healthsystem/impact_of_cons_availability/impact_of_pharmacist_intervention_hard_hr_constraints.py```
+
+"""
+
+from tlo import Date, logging
+from tlo.methods.fullmodel import fullmodel
+from tlo.scenario import BaseScenario
+
+
+class ImpactOfConsumablesAvailabilityIntervention(BaseScenario):
+    def __init__(self):
+        super().__init__()
+        self.seed = 0
+        self.start_date = Date(2010, 1, 1)
+        self.end_date = Date(2011, 2, 1) # 31/12/2025
+        self.pop_size = 100 # large population size for final simulation = 100,000
+        self.number_of_draws = 2  # <- one scenario
+        self.runs_per_draw = 3  # <- repeated this many times = 5
+
+    def log_configuration(self):
+        return {
+            'filename': 'impact_of_consumables_availability_intervention',
+            'directory': './outputs',  # <- (specified only for local running)
+            'custom_levels': {
+                '*': logging.WARNING,
+                'tlo.methods.demography': logging.INFO,
+                'tlo.methods.healthburden': logging.INFO,
+                "tlo.methods.healthsystem": logging.INFO,
+            }
+        }
+
+    def modules(self):
+        return fullmodel(resourcefilepath=self.resources)
+
+    def draw_parameters(self, draw_number, rng):
+        return {
+            'HealthSystem': {
+                'change_cons_availability_to': ['alternate_scenario1', 'NO_CHANGE'][draw_number],
+                'mode_appt_constraints': [2,2][draw_number]
+               }
+        }
+
+
+if __name__ == '__main__':
+    from tlo.cli import scenario_run
+
+    scenario_run([__file__])
