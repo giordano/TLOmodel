8f675ba830ee252f3a1bb26eb2864c00e5c16a33
tdm32 <t.mangal@imperial.ac.uk>
2023-05-22 12:26:28 +0100

    new functions to extract and summarise tuples from hiv logger

--- src/scripts/hiv/projections_jan2023/summarise_mihpsa_outputs.py
+++ src/scripts/hiv/projections_jan2023/summarise_mihpsa_outputs.py
@@ -28,14 +28,70 @@ outputspath = Path("./outputs/t.mangal@imperial.ac.uk")
 # 0) Find results_folder associated with a given batch_file (and get most recent [-1])
 results_folder = get_scenario_outputs("mihpsa_runs.py", outputspath)[-1]
 
-# look at one log (so can decide what to extract)
-log = load_pickled_dataframes(results_folder)
-log2 = load_pickled_dataframes(results_folder, draw=0, run=1, name=None)
-log3 = load_pickled_dataframes(results_folder, draw=0, run=2, name=None)
 
-baseline_outputs = log["tlo.methods.hiv"]["hiv_baseline_outputs"]
-detailed_outputs = log["tlo.methods.hiv"]["hiv_detailed_outputs"]
-deaths = log["tlo.methods.hiv"]["death"]
+# Collect results from each draw/run
+def extract_outputs(results_folder: Path,
+                    module: str,
+                    key: str,
+                    column: str) -> pd.DataFrame:
+    # get number of draws and numbers of runs
+    info = get_scenario_info(results_folder)
+
+    df_pop = pd.DataFrame()
+    df_prev = pd.DataFrame()
+    df_inf = pd.DataFrame()
+    df_dx = pd.DataFrame()
+    df_tx = pd.DataFrame()
+    df_vs = pd.DataFrame()
+    df_out = pd.DataFrame()
+
+    # 10 draws, 1 run
+    run = 0
+    for draw in range(info['number_of_draws']):
+        # load the log file
+        df: pd.DataFrame = load_pickled_dataframes(results_folder, draw, run, module)[module][key]
+        # first column is mean
+
+        tmp = pd.DataFrame(df[column].to_list(), columns=['pop', 'prev',
+                                                          "inf", "dx",
+                                                          "tx", "vs"])
+        df_pop[draw] = tmp["pop"]
+        df_prev[draw] = tmp["prev"]
+        df_inf[draw] = tmp["inf"]
+        df_dx[draw] = tmp["dx"]
+        df_tx[draw] = tmp["tx"]
+        df_vs[draw] = tmp["vs"]
+
+    df_out["mean_pop"] = df_pop.mean(axis=1)
+    df_out["mean_prev"] = df_prev.mean(axis=1)
+    df_out["mean_inf"] = df_inf.mean(axis=1)
+    df_out["mean_dx"] = df_dx.mean(axis=1)
+    df_out["mean_tx"] = df_tx.mean(axis=1)
+    df_out["mean_vs"] = df_vs.mean(axis=1)
+
+    return df_out
+
+
+baseline_all = extract_outputs(results_folder=results_folder,
+                               module="tlo.methods.hiv",
+                               key="hiv_baseline_outputs",
+                               column="outputs_age15_64")
+
+# extract pop size for M, F and Total
+# take mean and store
+
+# extract prevalence for M, F and Total
+# extract new infections for M, F, and Total
+# prop dx
+# if dx, % on tx
+# if tx, % VS
+
+
+# baseline_outputs = log["tlo.methods.hiv"]["hiv_baseline_outputs"]
+# detailed_outputs = log["tlo.methods.hiv"]["hiv_detailed_outputs"]
+# deaths = log["tlo.methods.hiv"]["death"]
+#
+
 
 # write to excel
 with pd.ExcelWriter(outputspath / ("MIHPSA_outputs2" + ".xlsx"), engine='openpyxl') as writer:
