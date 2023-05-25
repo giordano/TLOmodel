75847090ca006f6da9c93558749a547625d6b24c
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-05-25 17:10:43 +0100

    co_health scenario: till 2030

--- src/scripts/contraception/scenarios/run_analysis_co_health_impacts_no_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_co_health_impacts_no_diseases.py
@@ -73,7 +73,7 @@ class RunAnalysisCo(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2014, 12, 31),
+            end_date=Date(2030, 12, 31),
             initial_population_size=50_000,  # selected size for the Tim C at al. 2023 paper: 250K # TODO: update
             number_of_draws=1,  # <- one scenario
             runs_per_draw=10,  # <- repeated this many times
