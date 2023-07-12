228e6562724a3590ed555a31a8766111c3027df9
joehcollins <joseph.h.collins@ucl.ac.uk>
2023-07-12 11:48:33 +0100

    update calibration file

--- src/scripts/maternal_perinatal_analyses/calibration/standard_mph_calibration.py
+++ src/scripts/maternal_perinatal_analyses/calibration/standard_mph_calibration.py
@@ -29,10 +29,10 @@ from tlo.scenario import BaseScenario
 class Calibration(BaseScenario):
     def __init__(self):
         super().__init__()
-        self.seed = 6742
+        self.seed = 562661
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2021, 1, 1)
-        self.pop_size = 100_000
+        self.pop_size = 50_000
         self.number_of_draws = 1
         self.runs_per_draw = 20
 
