3b77168ac791fb66e9bb6d45551fdefc7b524564
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2023-08-31 15:44:38 +0100

    Increase pop size from local tests

--- src/scripts/healthsystem/impact_of_policy/scenario_impact_of_policy.py
+++ src/scripts/healthsystem/impact_of_policy/scenario_impact_of_policy.py
@@ -30,7 +30,7 @@ class ImpactOfHealthSystemMode(BaseScenario):
         self.seed = 0
         self.start_date = Date(2010, 1, 1)
         self.end_date = self.start_date + pd.DateOffset(years=33)
-        self.pop_size = 150
+        self.pop_size = 75_000
         self._scenarios = self._get_scenarios()
         self.number_of_draws = len(self._scenarios)
         self.runs_per_draw = 1
