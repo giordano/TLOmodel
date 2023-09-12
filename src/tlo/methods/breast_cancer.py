1702122256d9d79b9f161fd1395153d0d4c7b5d2
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-09-12 19:15:44 +0100

    brc, co: dummy examples added; if equip always same for HSI - set in __init__, otherwise updated in apply
    diff --git src/tlo/methods/breast_cancer.py src/tlo/methods/breast_cancer.py
    index 1ce9ad2bf..6225dab70 100644
    --- src/tlo/methods/breast_cancer.py
    +++ src/tlo/methods/breast_cancer.py
    @@ -646,6 +646,8 @@ class HSI_BreastCancer_Investigation_Following_breast_lump_discernible(HSI_Event
             self.TREATMENT_ID = "BreastCancer_Investigation"
             self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({"Over5OPD": 1, "Mammography": 1})
             self.ACCEPTED_FACILITY_LEVEL = '3'  # Biopsy only available at level 3 and above.
    +        self.EQUIPMENT = {'Slice Master sample processing Unit', 'Paraffin Dispense', 'Whatever used with biopsy'}
    +        # biopsy always performed with this HSI
    
         def apply(self, person_id, squeeze_factor):
             df = self.sim.population.props
    @@ -759,6 +761,9 @@ class HSI_BreastCancer_StartTreatment(HSI_Event, IndividualScopeEventMixin):
             df.at[person_id, "brc_date_treatment"] = self.sim.date
             df.at[person_id, "brc_stage_at_which_treatment_given"] = df.at[person_id, "brc_status"]
    
    +        # Update equipment
    +        self.EQUIPMENT.update({'Anything used for mastectomy as I guess this is about'})
    +
             # Schedule a post-treatment check for 12 months:
             hs.schedule_hsi_event(
                 hsi_event=HSI_BreastCancer_PostTreatmentCheck(
    diff --git src/tlo/methods/contraception.py src/tlo/methods/contraception.py
    index c9969916b..da1e2f3ba 100644
    --- src/tlo/methods/contraception.py
    +++ src/tlo/methods/contraception.py
    @@ -1280,6 +1280,12 @@ class HSI_Contraception_FamilyPlanningAppt(HSI_Event, IndividualScopeEventMixin)
    
                 _new_contraceptive = self.new_contraceptive
    
    +            # Update equipment
    +            if _new_contraceptive == 'female_sterilization':
    +                self.EQUIPMENT.update({'Smt used to sterilize a woman'})
    +            elif _new_contraceptive == 'IUD':
    +                self.EQUIPMENT.update({'Equipment used when performing IUD'})
    +
             else:
                 _new_contraceptive = "not_using"

--- src/tlo/methods/breast_cancer.py
+++ src/tlo/methods/breast_cancer.py
@@ -646,6 +646,8 @@ class HSI_BreastCancer_Investigation_Following_breast_lump_discernible(HSI_Event
         self.TREATMENT_ID = "BreastCancer_Investigation"
         self.EXPECTED_APPT_FOOTPRINT = self.make_appt_footprint({"Over5OPD": 1, "Mammography": 1})
         self.ACCEPTED_FACILITY_LEVEL = '3'  # Biopsy only available at level 3 and above.
+        self.EQUIPMENT = {'Slice Master sample processing Unit', 'Paraffin Dispense', 'Whatever used with biopsy'}
+        # biopsy always performed with this HSI
 
     def apply(self, person_id, squeeze_factor):
         df = self.sim.population.props
@@ -758,6 +760,9 @@ class HSI_BreastCancer_StartTreatment(HSI_Event, IndividualScopeEventMixin):
         # Record date and stage of starting treatment
         df.at[person_id, "brc_date_treatment"] = self.sim.date
         df.at[person_id, "brc_stage_at_which_treatment_given"] = df.at[person_id, "brc_status"]
+
+        # Update equipment
+        self.EQUIPMENT.update({'Anything used for mastectomy as I guess this is about'})
 
         # Schedule a post-treatment check for 12 months:
         hs.schedule_hsi_event(
