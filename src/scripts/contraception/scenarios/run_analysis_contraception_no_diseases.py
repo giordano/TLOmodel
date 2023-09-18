1ad5c1c929c640206a74842acf04e748b26c77c8
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-09-18 23:27:24 +0100

    run_analysis_contraception_no_dis: till 2050, 20K pop

--- src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
+++ src/scripts/contraception/scenarios/run_analysis_contraception_no_diseases.py
@@ -49,8 +49,8 @@ class RunAnalysisCo(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2099, 12, 31),
-            initial_population_size=250_000,  # selected size for the Tim C at al. 2023 paper: 250K
+            end_date=Date(2050, 12, 31),
+            initial_population_size=20_000,  # selected size for the Tim C at al. 2023 paper: 250K
             number_of_draws=1,  # <- one scenario
             runs_per_draw=1,  # <- repeated this many times
         )
