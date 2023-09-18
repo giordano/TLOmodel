d78f90b7a8bda0c2c118f3daa0b384909b54e6ae
Eva Janouskova <janouskova.eva.012@gmail.com>
2023-09-18 22:10:27 +0100

    run_scenario_co_health_impacts_no_dis: till 2016, 4K pop

--- src/scripts/contraception/scenarios/run_scenario_co_health_impacts_no_diseases.py
+++ src/scripts/contraception/scenarios/run_scenario_co_health_impacts_no_diseases.py
@@ -52,8 +52,8 @@ class RunAnalysisCo(BaseScenario):
         super().__init__(
             seed=0,
             start_date=Date(2010, 1, 1),
-            end_date=Date(2030, 12, 31),
-            initial_population_size=2_000,  # selected size for the Tim C at al. 2023 paper: 250K  #TODO: update
+            end_date=Date(2016, 12, 31),
+            initial_population_size=4_000,  # selected size for the Tim C at al. 2023 paper: 250K  #TODO: update
             number_of_draws=1,  # <- one scenario
             runs_per_draw=1,  # <- repeated this many times
         )
