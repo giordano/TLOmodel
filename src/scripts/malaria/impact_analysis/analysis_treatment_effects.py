559bf0f32dfc5e6c5eb7634fea3062c902b47b32
tdm32 <t.mangal@imperial.ac.uk>
2023-09-21 15:10:55 +0100

    set up scenarios

--- src/scripts/malaria/impact_analysis/analysis_treatment_effects.py
+++ src/scripts/malaria/impact_analysis/analysis_treatment_effects.py
@@ -42,8 +42,7 @@ class EffectOfProgrammes(BaseScenario):
         self.start_date = Date(2010, 1, 1)
         self.end_date = Date(2020, 1, 1)
         self.pop_size = 100_000
-        self._scenarios = self._get_scenarios()
-        self.number_of_draws = len(self._scenarios)
+        self.number_of_draws = 6
         self.runs_per_draw = 5
 
         self.treatment_effects = pd.read_excel(
@@ -70,86 +69,16 @@ class EffectOfProgrammes(BaseScenario):
         return fullmodel(resourcefilepath=self.resources)
 
     def draw_parameters(self, draw_number, rng):
-        return list(self._scenarios.values())[draw_number]
-
-    def _get_scenarios(self) -> Dict[str, Dict]:
-        """ create a dict which modifies the treatment effects and
-        health system settings for each scenario """
-
         return {
-            "Baseline":
-                mix_scenarios(
-                    get_parameters_for_status_quo(),
-                    {'HealthSystem': {'use_funded_or_actual_staffing': 'funded',
-                                      'mode_appt_constraints': 1,
-                                      },
-                     'Tb': {
-                         'scenario': 0,
-                     },
-                     }
-                ),
-
-            "Remove_HIV_effects":
-                mix_scenarios(
-                    get_parameters_for_status_quo(),
-                    {'HealthSystem': {'use_funded_or_actual_staffing': 'funded',
-                                      'mode_appt_constraints': 1,
-                                      },
-                     'Tb': {
-                         'scenario': 1,
-                     },
-                     }
-                ),
-
-            "Remove_TB_effects":
-                mix_scenarios(
-                    get_parameters_for_status_quo(),
-                    {'HealthSystem': {'use_funded_or_actual_staffing': 'funded',
-                                      'mode_appt_constraints': 1,
-                                      },
-                     'Tb': {
-                         'scenario': 2,
-                     },
-                     }
-                ),
-
-            "Remove_malaria_effects":
-                mix_scenarios(
-                    get_parameters_for_status_quo(),
-                    {'HealthSystem': {'use_funded_or_actual_staffing': 'funded',
-                                      'mode_appt_constraints': 1,
-                                      },
-                     'Tb': {
-                         'scenario': 3,
-                     },
-                     }
-                ),
-
-            "Baseline_mode2":
-                mix_scenarios(
-                    get_parameters_for_status_quo(),
-                    {'HealthSystem': {'use_funded_or_actual_staffing': 'funded',
-                                      'mode_appt_constraints': 2,
-                                      'policy_name': 'VerticalProgrammes',
-                                      },
-                     'Tb': {
-                         'scenario': 0,
-                     },
-                     }
-                ),
-
-            "No_treatment_mode2":
-                mix_scenarios(
-                    get_parameters_for_status_quo(),
-                    {'HealthSystem': {'use_funded_or_actual_staffing': 'funded',
-                                      'mode_appt_constraints': 2,
-                                      'policy_name': 'VerticalProgrammes',
-                                      },
-                     'Tb': {
-                         'scenario': 5,
-                     },
-                     }
-                ),
+            'HealthSystem': {
+                'use_funded_or_actual_staffing': 'funded',
+                'mode_appt_constraints': [1, 1, 1, 1, 2, 2][draw_number],
+                'policy_name': ['naive', 'naive', 'naive', 'naive', 'VerticalProgrammes', 'VerticalProgrammes'][
+                    draw_number],
+            },
+            'Tb': {
+                'scenario': [0, 1, 2, 3, 0, 5][draw_number],
+            },
         }
 
 
