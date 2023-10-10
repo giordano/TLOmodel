a48cc4f85f028ad56d68fdbebf87ef39a8f67ec8
Margherita Molaro <48129834+marghe-molaro@users.noreply.github.com>
2023-10-10 11:57:23 +0100

    Reduce logger size

--- src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
+++ src/scripts/calibration_analyses/scenarios/long_run_all_diseases.py
@@ -36,7 +36,7 @@ class LongRun(BaseScenario):
                 'tlo.methods.demography': logging.INFO,
                 'tlo.methods.demography.detail': logging.WARNING,
                 'tlo.methods.healthburden': logging.INFO,
-                'tlo.methods.healthsystem': logging.INFO,
+        #        'tlo.methods.healthsystem': logging.INFO,
                 'tlo.methods.healthsystem.summary': logging.INFO,
                 "tlo.methods.contraception": logging.INFO,
             }
